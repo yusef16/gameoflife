23bbbe818729cfae018980364faae218607a1374
yusef <abed.yusef21@gmail.com>
2020-10-06 20:24:12 +0300

    add button

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+let coloredCol = 0;
+let coloredRow = 0;
 
 
 
 
 
 
+function createTableInput(a,b){
+	
 
-function createTableInput(){
-	var row = document.getElementById('x').value;
-	var coll = document.getElementById('y').value;
+	
 	let table = document.createElement("TABLE");
+	table.classList.add("table-class");
 	document.body.appendChild(table);
-		for (let i=0; i<row; i++){
+		for (let i=0; i<a; i++){
 			let tr = document.createElement("TR");
 			table.appendChild(tr);
-			for(let j=0; j<coll; j++){
+			for(let j=0; j<b; j++){
 				let td = document.createElement("TD");
 				tr.appendChild(td)
 			
@@ -27,3 +30,21 @@ function createTableInput(){
 
 
 
+function submitform(){
+	var row = document.getElementById('x').value;
+	var coll = document.getElementById('y').value;
+
+	createTableInput(row,coll);
+	CreateControlButton();
+
+
+}
+
+function CreateControlButton() {
+  var btn = document.createElement("BUTTON");
+  btn.innerHTML = "NEXT";
+  document.body.appendChild(btn);
+  btn.addEventListener("click", function() {
+  	console.log("xd")
+  } )
+}
